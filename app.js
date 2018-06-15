8fbecac707ced5de4ee4e68fb1415449d5fdee7b
loren <lorenceye@gmail.com>
2018-06-15 12:10:45 -0400

    Switchoff 1

---
+++ app.js
@@ -0,0 +1,25 @@
+const morgan = require (`morgan`)
+const express = require (`express`)
+const bodyParser = require (`body-parser`)
+const { db } = require (`./models/index.js`)
+
+const app = express()
+
+const index = require (`./views/index.js`)
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
+app.use(morgan(`dev`))
+app.use(express.static(`public`))
+
+app.get(`/`, function (req, res) {
+	res.send(index.main())
+
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen (PORT)
