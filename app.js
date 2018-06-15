3d56a87b618a4d0bbc88676f4e4d450e8320692e
Graham Johnson <grahamscottjohnson@gmail.com>
2018-06-15 12:15:38 -0400

    removed node_modules

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
-//something for routing
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const index = require('./views/index')
 
 let app = express();
 
-app.use(morgan('dev'));
+app.use( morgan('dev') );
 app.use( express.static('public') );
+
+app.get( '/', function(req, res){
+    res.send( "Hello World" );
+} )
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen( PORT );
